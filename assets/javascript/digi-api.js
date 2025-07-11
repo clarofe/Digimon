e22af34017d995099e4c9fc3afd504180d2c7672
Fernanda Claro <fernandaclaroa@gmail.com>
2025-07-11 00:10:34 -0300

    digi-api.js

--- assets/javascript/digi-api.js
+++ assets/javascript/digi-api.js
@@ -1,35 +1,19 @@
+const digiApi = {};
 
-const digiApi = {}
-
-function convertPokeApiDetailToDigimon(digiDetail) {
-    const digimon = new digimon()
-    digimon.number = digiDetail.id
-    digimon.name = digiDetail.name
-
-    const types = digieDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    digimon.types = types
-    digimon.type = type
-
-    digimon.photo = digiDetail.sprites.other.dream_world.front_default
-
-    return digimon
+function convertDigiApiDetailToDigimon(digiDetail) {
+    const digimon = new Digimon();
+    digimon.name = digiDetail.name;
+    digimon.img = digiDetail.img;
+    digimon.level = digiDetail.level;
+    
+    return digimon;
 }
 
-digiApi.getDigimonDetail = (digimon) => {
-    return fetch(digimon.url)
-        .then((response) => response.json())
-        .then(convertDigiApiDetailToDigimon)
-}
-
-digiApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://digi-api.com/v1/digimon/?offset=${offset}&limit=${limit}`
+digiApi.getDigimons = () => {
+    const url = 'https://digimon-api.vercel.app/api/digimon';
 
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((digimons) => digimons.map(digiApi.getDigimonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((digimonsDetails) => digimonsDetails)
+        .then((jsonBody) => jsonBody.map(convertDigiApiDetailToDigimon))
+        .catch((error) => console.error(error));
 }
